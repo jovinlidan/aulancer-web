4869843b1c474029015a004035d2ec874279e322
Jopp <jovinlidan1@gmail.com>
2023-11-10 02:11:57 +0700

    refine chat with us comp

--- src/modules/home/chat-with-us/chat-with-us.tsx
+++ src/modules/home/chat-with-us/chat-with-us.tsx
@@ -5,7 +5,7 @@ import LineDecorations from "./components/line-decorations";
 export default function ChatWithUs() {
   return (
     <Section className="bg-gradient-secondary-bg rounded-2xl flex flex-col md:flex-row justify-between items-center h-[300px] md:h-[260px] lg:h-[285px] md:px-[20px] lg:px-[88px]">
-      <h4 className="bg-gradient-dark-bg bg-clip-text text-transparent font-bold text-lg md:text-2xl lg:text-4xl max-w-[531px] md:leading-[55px] max-md:mt-8 max-md:text-center">
+      <h4 className="bg-gradient-dark-bg bg-clip-text text-transparent font-bold text-lg md:text-2xl lg:text-4xl max-w-[531px] md:!leading-[40px] lg:!leading-[55px] max-md:mt-8 max-md:text-center">
         Let’s collaborate together to achieve our goals!
       </h4>
       <div className="relative max-md:mb-20">
