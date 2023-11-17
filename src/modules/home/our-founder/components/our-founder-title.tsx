f65f45f2aa13c79745f7f5c37209a0e2930e5b4c
jop <springkraf>
2023-11-17 16:43:12 +0700

    fix some issue on heading tag

--- src/modules/home/our-founder/components/our-founder-title.tsx
+++ src/modules/home/our-founder/components/our-founder-title.tsx
@@ -4,12 +4,12 @@ export default function OurFounderTitle() {
   return (
     <div className="flex flex-col items-center max-md:mb-4 md:mb-14">
       <hr className="bg-gradient-service-text w-[40px] md:w-[69px] h-[5px]" />
-      <h5 className="text-gray-900 text-center font-normal text-3xl lg:text-4xl mt-5">
+      <h1 className="text-gray-900 text-center font-normal text-3xl lg:text-4xl mt-5">
         Get close to
-      </h5>
-      <h5 className="text-gray-900 text-center font-bold text-3xl lg:text-4xl mt-4">
+      </h1>
+      <h1 className="text-gray-900 text-center font-bold text-3xl lg:text-4xl mt-4">
         Our Founder
-      </h5>
+      </h1>
     </div>
   );
 }
--- src/modules/home/our-key/components/our-key-title.tsx
+++ src/modules/home/our-key/components/our-key-title.tsx
@@ -4,12 +4,12 @@ export default function OurKeyTitle() {
   return (
     <div className="flex flex-col items-center max-md:mb-4 md:mb-14">
       <hr className="bg-gradient-service-text w-[40px] md:w-[69px] h-[5px]" />
-      <h5 className="text-gray-900 text-center font-normal text-3xl lg:text-4xl mt-3">
+      <h1 className="text-gray-900 text-center font-normal text-3xl lg:text-4xl mt-3">
         Our
-      </h5>
-      <h5 className="text-gray-900 text-center font-bold text-3xl lg:text-4xl mt-1">
+      </h1>
+      <h1 className="text-gray-900 text-center font-bold text-3xl lg:text-4xl mt-1">
         Key Values
-      </h5>
+      </h1>
     </div>
   );
 }
